13a00e21845f3216032553903624b28a91065a57
Markus Hinz <markus.hinz@tum.de>
2020-04-19 16:38:21 +0200

    back to dockerhub

--- publi.sh
+++ publi.sh
@@ -10,9 +10,9 @@ CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -o "dist/$version/aws-ses-pop3-s
 git fetch
 git tag "v$version" -m "v$version"
 
-docker build -t "docker.pkg.github.com/markushinz/aws-ses-pop3-server/aws-ses-pop3-server:$version" \
-  -t "docker.pkg.github.com/markushinz/aws-ses-pop3-server/aws-ses-pop3-server:latest" .
-docker push "docker.pkg.github.com/markushinz/aws-ses-pop3-server/aws-ses-pop3-server:$version"
-docker push "docker.pkg.github.com/markushinz/aws-ses-pop3-server/aws-ses-pop3-server:latest"
+docker build -t "markushinz/aws-ses-pop3-server:$version" \
+  -t "markushinz/aws-ses-pop3-server:latest" .
+docker push "markushinz/aws-ses-pop3-server:$version"
+docker push "markushinz/aws-ses-pop3-server:latest"
 
 git push origin "v$version"
